6a7e93bdc775b2c41df6a5035c76909fe70d974a
khutamgiacvang-tech <khutamgiacvang@gmail.com>
2026-09-06 20:35:18 +0700

    dasw

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("../controllers/homeController");
+const path = require("path");
+
+router.get("/download/manganest.apk", (req, res) => {
+  const apkPath = path.join(__dirname, "../public/download/manganest.apk");
+  return res.download(apkPath, "MangaNest.apk", {
+    headers: {
+      "Content-Type": "application/vnd.android.package-archive",
+      "Cache-Control": "public, max-age=3600"
+    }
+  }, (err) => {
+    if (err && !res.headersSent) {
+      console.error("APK DOWNLOAD ERROR:", err);
+      return res.status(404).send("Không tìm thấy file APK.");
+    }
+  });
+});
 
 router.get("/", homeController.home);
 
